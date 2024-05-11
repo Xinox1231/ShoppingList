30d05823002a72c880f336f7999c76113c2bf3af
Xinox3213 <mavrinonlywork@gmail.com>
2024-05-11 18:43:05 +0700

    Базовые методы добавлены в Domain слой, data содержит реализацю

--- app/src/main/java/com/bignerdranch/android/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/shoppinglist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bignerdranch.android.shoppinglist
+package com.bignerdranch.android.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bignerdranch.android.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
