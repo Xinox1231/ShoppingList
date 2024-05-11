9d788e55c429e7ec6b59784fa8010dfe7ff57aac
Xinox3213 <mavrinonlywork@gmail.com>
2024-05-11 18:43:38 +0700

    Базовые методы добавлены в Domain слой, data содержит реализацию

--- app/src/main/java/com/bignerdranch/android/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/bignerdranch/android/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -11,6 +11,7 @@ object ShopListRepositoryImpl : ShopListRepository {
     override fun addShopItem(shopItem: ShopItem) {
         if(shopItem.id == ShopItem.UNDEFINED_ID){
             shopItem.id = autoIncrementId++
+
         }
         shopList.add(shopItem)
     }
